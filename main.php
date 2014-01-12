eb7a1fb22d9ca804c5be8a97c69c4cafd437d45e
Ross Whisson <ross@joineo.com>
2014-01-12 14:33:30 -0800

    added robots.txt

---
+++ main.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Joineo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Life+Savers:400,700' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <style>
+    html, body, .wrapper {
+    	height: 100%;
+    	min-height: 100%;
+    	width: 100%;
+    }
+
+    .wrapper {
+    	display: table;
+		text-align: center;
+    }
+
+    .title {
+    	font-family: 'Life Savers', cursive;
+    	font-weight: bold;
+    	font-size: 100px;
+    	display: table-cell;
+  		vertical-align: middle;
+  		width: 300px;
+  		text-align: center;
+    }
+    </style>
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-46998529-1', 'joineo.com');
+	  ga('send', 'pageview');
+
+	</script>
+  </head>
+  <body>
+  	<div class="wrapper">
+    	<div class="title">Joineo</div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+  </body>
+</html>
